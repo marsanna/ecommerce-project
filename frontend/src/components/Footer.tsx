0de57a15fbaf87f8a991c3f7d8c26255f06414e3
Anna Tsyryatyeva <anny-b@yandex.ru>
2026-02-16 13:50:10 +0100

    Authentification bug fixed

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,6 +3,9 @@ import { Link } from "react-router";
 function Footer() {
   return (
     <footer className="mt-10 bg-gray-900 py-10 text-gray-300">
+      <Link to="/contact" className="transition hover:text-gray-400">
+        Contact
+      </Link>
       <div className="mt-5 text-center text-sm text-gray-500">
         © 2026 My Shop. All rights reserved.
       </div>
